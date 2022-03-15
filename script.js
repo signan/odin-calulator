9a9935173b072cdafdd0e03a54feafc0d1cfd4d4
signan <signmeup132@gmail.com>
2022-03-15 22:48:36 +0300

    create operate function

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(a, b, operator) {
+    switch(operator) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return subtract(a, b);
+            break;
+        case '*':
+            return multiply(a, b);
+            break;
+        case '/':
+            return divide(a, b);
+            break;
+        default:
+            console.log('not a number');
+    }
+}
+
+console.log(operate(5, 5, '/'))
