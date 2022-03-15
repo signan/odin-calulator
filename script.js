32eee81644b93cfad2b3c64bc89668edbc5c7700
signan <signmeup132@gmail.com>
2022-03-15 22:42:58 +0300

    add basic math operations

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello maker")
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
