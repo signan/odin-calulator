071e909df89ef8d1aca69a8daf8db5e705dc857b
signan <signmeup132@gmail.com>
2022-03-20 16:11:59 +0300

    wire clear, del, number and operation buttons to functions

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let displayText = '';
+const display = document.querySelector('.display > h1')
+
+
 
 function add(a, b) {
     return a + b;
@@ -34,4 +38,61 @@ function operate(a, b, operator) {
     }
 }
 
-console.log(operate(5, 5, '/'))
+function wireButtons() {
+    let buttons = document.querySelectorAll(".btn");
+    buttons.forEach(btn => {
+        let btnClasses = btn.classList;
+        if (btnClasses.contains('op')) {
+            btn.addEventListener('click', addOperation);
+        }
+        else if (btnClasses.contains('clr')) {
+            btn.addEventListener('click', clearDisplay);
+        }
+        else if (btnClasses.contains('del')) {
+            btn.addEventListener('click', del);
+        }
+        else {
+            btn.addEventListener('click', addDigit);
+        }
+    });
+}
+
+function addDigit() {
+    displayText = displayText + event.currentTarget.textContent;
+    updateDisplay();
+}
+
+function addOperation() {
+    if (endsWithOperation()) {
+        del();
+    }
+    displayText = displayText + event.currentTarget.textContent;
+    updateDisplay();
+}
+
+function endsWithOperation() {
+    let operations = ['+', '-', '*', '/']
+    for (let operation of operations) {
+        if(displayText.endsWith(operation))
+            return true;
+    }
+    return false;
+}
+
+
+function clearDisplay() {
+    displayText = ''
+    updateDisplay();
+}
+
+function del() {
+    displayText = displayText.slice(0, -1);
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.textContent = displayText;
+}
+
+
+wireButtons();
