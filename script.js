3715cda7feb7c370c1b50272c86c744f348739f8
signan <signmeup132@gmail.com>
2022-03-24 18:48:18 +0300

    start major rework for logic

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-let displayText = '';
-const display = document.querySelector('.display > h1')
-let currentNumber = '';
+const currentScreen = document.querySelector('.display > h1')
+const fullScreen = document.querySelector('.display > h3')
 let num1 = '';
 let num2 = '';
 let result = ''
 let operator = '';
+
+const numberButtons = document.querySelectorAll(".btn.num");
+const operatorButtons = document.querySelectorAll(".btn.op");
+const clearButton = document.getElementById("btn-clear");
+const deleteButton = document.querySelectorAll("btn-delete");
+const equalsButton = document.querySelectorAll("btn-equals");
+const pointButton = document.querySelectorAll("btn-point");
+console.log(operatorButtons);
+
+equalsButton.addEventListener('click', evaluate);
+clearButton.addEventListener('click', clear);
+deleteButton.addEventListener('click', deleteNumber);
+pointButton.addEventListener('click', appendPoint);
+numberButtons.forEach((button) => button.addEventListener('click', () => appendNumber(button.textContent)));
+operatorButtons.forEach((button) => button.addEventListener('click', () => setOpertaion(button.textContent)));
+
+
+
 
 
 function add(a, b) {
@@ -24,56 +41,56 @@ function divide(a, b) {
 }
 
 function operate(a, b, operator) {
+    // convert a and b to integers
     switch(operator) {
         case '+':
             return add(a, b);
-            break;
         case '-':
             return subtract(a, b);
-            break;
         case '*':
             return multiply(a, b);
-            break;
         case '/':
             return divide(a, b);
-            break;
-        default:
-            console.log('not a number');
     }
 }
 
-function wireButtons() {
-    let buttons = document.querySelectorAll(".btn");
-    buttons.forEach(btn => {
-        let btnClasses = btn.classList;
-        if (btnClasses.contains('op')) {
-            btn.addEventListener('click', addOperation);
-        }
-        else if (btn.classList.contains('eq')) {
-            btn.addEventListener('click', calculate);
-        }
-        else if (btnClasses.contains('clr')) {
-            btn.addEventListener('click', clearDisplay);
-        }
-        else if (btnClasses.contains('del')) {
-            btn.addEventListener('click', del);
-        }
-        else {
-            btn.addEventListener('click', addDigit);
-        }
-    });
-}
-
-function addDigit() {
+function appendNumber() {
+    if (result) {
+        clear();
+        result = null;
+    }
     displayText = displayText + event.currentTarget.textContent;
     currentNumber = currentNumber + event.currentTarget.textContent;
-    console.log(currentNumber)
-    updateDisplay();
+    console.log(displayText)
+    updateScreen();
 }
 
-function addOperation() {
+function setOperation() {
     if (endsWithOperation()) {
-        del();
+        console.log('del')
+
+        deleteNumber();
+    }
+
+    if (displayText === '' & event.currentTarget.textContent != '-') {
+        return;
+    }
+
+    if (displayText === '') {
+        if (event.currentTarget.textContent === '-') {
+            if (currentNumber.startsWith('-')) {
+                return;
+            }
+            displayText = displayText + event.currentTarget.textContent;
+            currentNumber = currentNumber + event.currentTarget.textContent;
+            updateScreen();
+            return;
+        }
+        return;
+    }
+
+    if(operator != '') {
+        evaluate()
     }
 
     if (num1 === '') {
@@ -87,10 +104,13 @@ function addOperation() {
 
     operator = event.currentTarget.textContent;
     displayText = displayText + operator;
-    updateDisplay();
+    updateScreen();
 }
 
-function calculate() {
+function evaluate() {
+    if (operator === '') {
+        return;
+    }
     if (num2 === '') {
         num2 = currentNumber;
         currentNumber = '';
@@ -99,6 +119,9 @@ function calculate() {
     console.log(`num1 ${num1} num2 ${num2} operator ${operator} result:${result}`)
     num1 = result;
     num2 ='';
+    operator = '';
+    displayText = String(result);
+    updateScreen();
 
 }
 
@@ -124,20 +147,20 @@ function endsWithOperation() {
 }
 
 
-function clearDisplay() {
+function clear() {
     currentNumber = '';
     num1 = '';
     num2 = '';
     displayText = '';
-    updateDisplay();
+    updateScreen();
 }
 
-function del() {
+function deleteNumber() {
     displayText = displayText.slice(0, -1);
-    updateDisplay();
+    updateScreen();
 }
 
-function updateDisplay() {
+function updateScreen() {
     display.textContent = displayText;
 }
 
